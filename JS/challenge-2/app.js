efd4dce6a8584965b338d5de4ce01f527ce0f98e
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-02-23 19:01:20 +0530

    js dom challange 2 completed

--- JS/challenge-2/app.js
+++ JS/challenge-2/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+
+let redButton = document.getElementById("redButton");
+let greenButton = document.getElementById("greenButton");
+let blueButton = document.getElementById("blueButton");
+let purpleButton = document.getElementById("purpleButton");
+let resetButton = document.getElementById("resetButton");
+let mainHeading = document.getElementById("mainHeading");
+
+redButton.addEventListener("click", () => {
+  mainHeading.style.color = "red";
+});
+greenButton.addEventListener("click", () => {
+  mainHeading.style.color = "green";
+});
+blueButton.addEventListener("click", () => {
+  mainHeading.style.color = "blue";
+});
+purpleButton.addEventListener("click", () => {
+  mainHeading.style.color = "purple";
+});
+resetButton.addEventListener("click", () => {
+  mainHeading.style.color = "black";
+});
