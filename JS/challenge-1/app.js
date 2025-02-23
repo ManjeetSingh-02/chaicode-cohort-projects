391045034e9d71177ff124d4f77acf88868169e5
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-02-23 18:47:59 +0530

    dom challange 1 completed

--- JS/challenge-1/app.js
+++ JS/challenge-1/app.js
@@ -1,3 +1,22 @@
 /**
  * Write your challenge solution here
  */
+
+let body = document.getElementById("body");
+let btn = document.getElementById("toggleButton");
+let statusp = document.getElementById("status");
+let bulb = document.getElementById("bulb");
+
+btn.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    statusp.textContent = "Status: Off";
+    btn.textContent = "Turn On";
+    bulb.classList.add("off");
+  } else {
+    body.classList.add("dark-mode");
+    btn.textContent = "Turn Off";
+    statusp.textContent = "Status: On";
+    bulb.classList.remove("off");
+  }
+});
