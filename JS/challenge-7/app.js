2f70b09cb62d74e5bb1546ae5940f7e671965c7a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-02-25 11:06:01 +0530

    js dom challange 7 completed

--- JS/challenge-7/app.js
+++ JS/challenge-7/app.js
@@ -1,3 +1,15 @@
 /**
  * Write your challenge solution here
  */
+
+let accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach((accordionItem) => {
+  accordionItem.addEventListener("click", () => {
+    let activeAccordionItem = document.querySelector(".accordion-item.active");
+
+    if (activeAccordionItem && activeAccordionItem !== accordionItem) activeAccordionItem.classList.remove("active");
+
+    accordionItem.classList.toggle("active");
+  });
+});
