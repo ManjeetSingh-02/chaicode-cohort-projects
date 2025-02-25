f0e97e83bc91e8746501054d4e9fd6268a97d8bf
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-02-25 13:00:20 +0530

    js dom challange 9 completed

--- JS/challenge-9/app.js
+++ JS/challenge-9/app.js
@@ -1,3 +1,25 @@
 /**
  * Write your challenge solution here
  */
+
+let toggleBtn = document.getElementsByClassName("toggle-btn");
+let closeBtn = document.getElementsByClassName("close-btn");
+let panel = document.getElementsByClassName("panel");
+let menuItems = document.querySelectorAll(".menu-item");
+
+toggleBtn.item(0).addEventListener("click", () => {
+  panel.item(0).classList.toggle("active");
+  toggleBtn.item(0).innerText = toggleBtn.item(0).innerText === "Open Menu" ? "Close Menu" : "Open Menu";
+});
+
+closeBtn.item(0).addEventListener("click", () => {
+  panel.item(0).classList.remove("active");
+  toggleBtn.item(0).innerText = "Open Menu";
+});
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    alert("You clicked on " + menuItem.innerText);
+    panel.item(0).classList.remove("active");
+  });
+});
